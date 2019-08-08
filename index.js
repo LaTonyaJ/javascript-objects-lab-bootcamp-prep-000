8a33fed3ad3202cd560047a593f0fdb623d6e9b8
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-08-08 18:10:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function deleteFromObjectByKey(object, key){
 function destructivelyDeleteFromObjectByKey(object, key){
   
   delete object.key
-  
+  object = Object.assign({})
   var newObj = Object.assign({})
   return newObj
   
